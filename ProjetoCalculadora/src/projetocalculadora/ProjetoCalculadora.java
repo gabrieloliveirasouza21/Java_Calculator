82686818b48ccd127c44e9cf32ea906cd32a35de
Hugodias95 <100964931+Hugodias95@users.noreply.github.com>
2022-08-23 21:17:22 -0300

    Atributos/Métodos
    Tela re-feita

---
+++ ProjetoCalculadora/src/projetocalculadora/ProjetoCalculadora.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetocalculadora;
+
+/**
+ *
+ * @author gabri
+ */
+public class ProjetoCalculadora {
+    
+    //Atributos
+    private String num1 = "";
+    private String num2 = "";
+    private double numAux1 = 0;
+    private double numAux2 = 0;
+    private String vet[] = new String[2];
+    
+    //Métodos
+
+    //Get+Set para num1 e num2
+    //Set para numAux1, numAux2 e String vet[2]
+    
+    public String getNum1() {
+        return num1;
+    }
+
+    public void setNum1(String num1) {
+        this.num1 = num1;
+    }
+
+    public String getNum2() {
+        return num2;
+    }
+
+    public void setNum2(String num2) {
+        this.num2 = num2;
+    }
+
+    public void setNumAux1(double numAux1) {
+        this.numAux1 = numAux1;
+    }
+
+    public void setNumAux2(double numAux2) {
+        this.numAux2 = numAux2;
+    }
+
+    public void setVet(String[] vet) {
+        this.vet = vet;
+    }
+    
+    //Calculando resultados
+    
+    
+    } 
